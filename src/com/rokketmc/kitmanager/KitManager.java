661a261c7e3c94a6aaef3e17278902314fc37eac
Shadowwolf97 <xXShadowwolf97Xx@gmail.com>
2014-11-29 15:15:19 -0500

    Removed static methods from the library to allow for multiple instances.

--- src/com/rokketmc/kitmanager/KitManager.java
+++ src/com/rokketmc/kitmanager/KitManager.java
@@ -7,19 +7,13 @@ import java.util.HashMap;
 public class KitManager {
 
     private HashMap<String, Kit> kits = new HashMap();
-    private static KitManager instance;
 
-    public static Kit getKit(String name) {
-        return getKitManager().kits.get(name.toLowerCase());
+    public Kit getKit(String name) {
+        return kits.get(name.toLowerCase());
     }
 
-    public static void addKit(Kit kit) {
-        getKitManager().kits.put(kit.getName(), kit);
+    public void addKit(Kit kit) {
+        kits.put(kit.getName(), kit);
     }
 
-    private static KitManager getKitManager() {
-        if(instance == null)
-            instance = new KitManager();
-        return instance;
-    }
 }
